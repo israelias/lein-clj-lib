8f21be066c2cf9653b404aeae56e51163a5c3565
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-08 20:02:34 +0800

    Configure project properties and main class

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Environment to run Clojure with Java/Scala in a mono Leiningen repo"
+  :url "https://github.com/israelias/lein-clj-lib"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns app.core})
+  :main ^:skip-aot app.core
+  :target-path "target/%s" 
+  :repl-options {:init-ns app.core}
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}) 
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,6 +1,7 @@
-(ns app.core)
+(ns app.core
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "About to know a whole lot."
+  [& args]
+  (println "About these args:" args))
