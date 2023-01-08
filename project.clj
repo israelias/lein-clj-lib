904b1b0ca6f4d2cd215cd2b9dfb2d81b55b200b5
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-08 20:19:37 +0800

    Run app with cheshire

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/israelias/lein-clj-lib"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.11.1"]
+                 [cheshire "5.11.0"]
+                 ]
   :main ^:skip-aot app.core
   :target-path "target/%s" 
   :repl-options {:init-ns app.core}
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,7 +1,10 @@
 (ns app.core
+  (:require [cheshire.core :as cheshire])
   (:gen-class))
 
 (defn -main
   "About to know a whole lot."
   [& args]
-  (println "About these args:" args))
+  (println "About these args?:" args)
+  (println (cheshire/generate-string {:name "Elias" :size "venti"}))
+  )
