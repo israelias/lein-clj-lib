4ce69edb4c5cea5053a5faa53d4f61f1d8c0658e
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-08 20:09:35 +0800

    Add package deps info

---
+++ lib/deps/package-management.clj
@@ -0,0 +1,14 @@
+;; Declarative Dep Mgmt
+
+; Declare everything in project.clj
+; name, version and optional metadata
+; clj is a hosted lang in the jvm like java jars
+; repos just like the dep vectors
+
+;; :dependencies [[org.clojure/clojure "1.11.1"]
+;;                [org.jclouds/jclouds "1.0" :classifier "jdk11"]
+;;                [net.sf.ehcache/ehcache "2.3.1" :extension "pom"]]
+               
+;;  :repositories [["java.net" "https://download.java.net/maven/2"]
+;;                 ["sonatype" {url: "https://oss.sonatype.org/content/repositories/releases"}]
+;;                 ]
